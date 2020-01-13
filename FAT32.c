abc91578902ce9c279ea95a639fc986a7df27047
shiva-prasad-reddy <shiva-prasad-reddy>
2020-01-13 21:04:49 +0530

    --

--- FAT32.c
+++ FAT32.c
@@ -25,6 +25,11 @@ struct DIRECTORY_ENTRY {
 	struct DIRECTORY_ENTRY *dir;
 };
 
+struct FSInfo {
+	unsigned int Free_Count;
+	unsigned int Nxt_Free;
+};
+
 //To display a part of the buffer
 void display_series_of_characters(char *buffer, int length)
 {
@@ -142,12 +147,15 @@ struct DIRECTORY_ENTRY* readDirecory(int readingPoint, int clusterNumber, int di
 		//End Of Records
 		if(buffer[32 * i] == 0x0) break;
 
-		//printf("Unused Record caused By Deletion
+		//Unused Record caused By Deletion
 		if(buffer[32 * i] == 0xE5) continue;
 
+		printf("%d  occupied\n", i);
 		int attr = turn_to_integer(buffer + (32 * i), 11, 1);
 		int X;
-
+		printf("CHECK THESE ==> ");
+		display_series_of_characters(buffer + (32 * i), 11);
+		printf("\n");
 		//LONG FILE NAME ENTRY
 		if(attr == 0xF)
 		{
@@ -261,14 +269,224 @@ void displayDIREntries(char *tab, struct DIRECTORY_ENTRY *temp, unsigned int *FA
 	}
 }
 
+void displayFSInfo(struct FSInfo *fsinfo)
+{
+	printf("\n+++  FSInfo +++\n");
+	printf("Free Cluster Count = %d\n", fsinfo->Free_Count);
+	printf("First Available free cluster = %d\n", fsinfo->Nxt_Free);
+	printf("++++++++++++++\n");
+}
+
+void initializeFSInfo(int disk, struct FSInfo *fsinfo)
+{
+	unsigned char buffer[512];
+	lseek(disk, 512, SEEK_SET);
+	read(disk, buffer, 512);
+	fsinfo->Free_Count = turn_to_integer(buffer, 488, 4);
+	fsinfo->Nxt_Free = turn_to_integer(buffer, 492, 4);
+
+	displayFSInfo(fsinfo);
+
+}
+
+
+
+
+void storeDirEntryInBuffer(char *buffer, char *Name, unsigned short int Attr, unsigned int N)
+{
+	//Name
+	int nameLength = sizeof(Name)/sizeof(char);
+	for(int i = 0; i < 11; i++)
+	{
+		if(i < nameLength)
+			buffer[i] = Name[i];
+		else
+			buffer[i] = 32; //trailing space padded
+	}
+
+	//Attr
+	buffer[11] = Attr;
+
+	//NTRes
+	buffer[12] = 0;
+
+	//CrtTimeTenth
+	buffer[13] = 0;
+
+	//CrtTime
+	buffer[14] = 0;
+	buffer[15] = 0;
+
+	//CrtDate
+	buffer[16] = 0;
+	buffer[17] = 0;
+
+	//LstAccDate
+	buffer[18] = 0;
+	buffer[19] = 0;
+
+	//FstClusHI
+	buffer[20] = (N >> 16) & 0xFF;
+	buffer[21] = (N >> 24) & 0xFF;
+
+	//WrtTime
+	buffer[22] = 0;
+	buffer[23] = 0;
+
+	//WrtDate
+	buffer[24] = 0;
+	buffer[25] = 0;
+
+	//FstClusLO
+	buffer[26] = N & 0xFF;
+	buffer[27] = (N >> 8) & 0xFF;
+
+	buffer[28] = 0;
+	buffer[29] = 0;
+	buffer[30] = 0;
+	buffer[31] = 0;
+}
+
+int findFreeCluster(int *FAT, int FATEntryCount)
+{
+	//I'm skipping first 10 and Last 20 entries
+	for(int i = 10; i < (FATEntryCount - 20); i++)
+		if(FAT[i] == 0)
+			return i;
+}
+
+
+
+
+void createDir(int disk, char *Name, struct BPB *bpb, unsigned int *FAT, int FATEntryCount, int curDirClusNum)
+{
+
+	//code should be optimized for every directory and accessed byte wise or sector wise
+	//and remove redundant buffers
+	char buffer_1[32], buffer_2[32], buffer_3[32];
+
+	int freeCluster = findFreeCluster(FAT, FATEntryCount);
+
+
+	printf("Free Cluster = %d\n", freeCluster);
+	//entry in root dir
+	storeDirEntryInBuffer(buffer_1, Name, 0x10, freeCluster);
+
+	printf("SEE THIS --> ");
+	display_series_of_characters(buffer_1, 11);
+	printf("\n");
+
+	//entries in the created dir
+	storeDirEntryInBuffer(buffer_2, ".", 0x10, freeCluster);
+	storeDirEntryInBuffer(buffer_3, "..", 0x10, curDirClusNum); //curDirClusNum is 0 for root directory
+
+
+	unsigned long int occupied = 268435455;
+	unsigned char buffer_fat_entry[4];
+	buffer_fat_entry[0] = (occupied) & 0xFF;
+	buffer_fat_entry[1] = (occupied >> 8) & 0xFF;
+	buffer_fat_entry[2] = (occupied >> 16) & 0xFF;
+	buffer_fat_entry[3] = (occupied >> 24) & 0xFF;
+
+
+
+	//make that freecluster entry in fat to occupied (both FAT's)
+	lseek(disk, (bpb->RsvdSecCnt * bpb->BytsPerSec) + (freeCluster * 4), SEEK_SET);
+	write(disk, buffer_fat_entry, 4);
+	lseek(disk, bpb->RsvdSecCnt * bpb->BytsPerSec + (bpb->FATSz32 * bpb->BytsPerSec) + (freeCluster * 4), SEEK_SET);
+	write(disk, buffer_fat_entry, 4);
+
+	/*
+	unsigned char *buffer = malloc((bpb->FATSz32 * bpb->BytsPerSec) * sizeof(unsigned char));
+	read(disk, buffer, bpb->FATSz32 * bpb->BytsPerSec);
+	int NumberOfFATEntries = (bpb->FATSz32 * bpb->BytsPerSec) / 4;
+	
+	*/
+
+	//find a free entry in the curDir and copy the buffer_1
+
+
+
+
+
+
+
+	int startSector = getFirstSectorOfCluster(curDirClusNum, bpb);
+	//Root directory entries are of 32 Bytes
+	//and their number of entries are limited to the size of cluster
+	//and size is based on the FAT entries
+
+	int DirEntriesInACluster = (bpb->SecPerClus * bpb->BytsPerSec) / 32;
+	unsigned char *buffer = malloc((bpb->SecPerClus * bpb->BytsPerSec) * sizeof(unsigned char));
+
+	lseek(disk, startSector * bpb->BytsPerSec, SEEK_SET);
+	read(disk, buffer, (bpb->SecPerClus * bpb->BytsPerSec));
+
+	//readingPoint is used to skip these records in a directory
+	// . --> points to the same directory
+	// .. --> points to the parent directory
+	unsigned int fre;
+	for(int i = 0; i < DirEntriesInACluster; i++)
+	{
+		//printf("Chekc this out == >");
+		//display_series_of_characters(buffer + (32 * i), 11);
+		//printf("\n");
+		//End Of Records && Unused Record caused By Deletion
+		if(buffer[32 * i] == 0x0 || buffer[32 * i] == 0xE5)
+		{
+			fre = i;
+			break;
+		}
+	}
+
+	printf("Free Entry = %d\n", fre);
+	for(int i = 0; i < 32; i++)
+	{
+		buffer[(32 * fre) + i] = buffer_1[i];
+	}
+	
+	lseek(disk, startSector * bpb->BytsPerSec, SEEK_SET);
+	write(disk, buffer, (bpb->SecPerClus * bpb->BytsPerSec));
+
+
+
+
+
+
+	//go that free cluster and copy 0 inside te whole cluster(not efficient)
+	//and place the entries buffer_2 and buffer_3
+	unsigned int X = getFirstSectorOfCluster(freeCluster, bpb);
+	lseek(disk, X * bpb->BytsPerSec, SEEK_SET);
+	//make all entries 0;
+	unsigned char *cluster_buffer = calloc(bpb->SecPerClus * bpb->BytsPerSec, sizeof(unsigned char));
+	//copy first two entries
+	for(int i = 0; i < 32; i++)
+	{
+		cluster_buffer[i] = buffer_2[i];
+		cluster_buffer[32+i] = buffer_3[i];
+	}
+
+	write(disk, cluster_buffer, bpb->SecPerClus * bpb->BytsPerSec);
+
+
+	free(buffer);
+	free(cluster_buffer);
+	
+}
+
+
+
 int main()
 {
 
-	int disk = open("/dev/sdb1", O_RDONLY);
+	int disk = open("/dev/sdc1", O_RDWR);
 
 	//BIOS parametric Block
 	struct BPB bpb;
 	initializeBPB(disk, &bpb);
+
+	struct FSInfo fsinfo;
+	initializeFSInfo(disk, &fsinfo);
 
 
 	//Get FAT(file allocation table) into Memory
@@ -277,11 +495,11 @@ int main()
 	FAT = (unsigned int *)calloc(NumberOfFATEntries, sizeof(unsigned int));
 	initializeFAT32(disk, FAT, &bpb);
 
-	/*
-	printf("%d == %d\n",NumberOfFATEntries, 1988480);
+	
+	/*printf("%d == %d\n",NumberOfFATEntries, 1988480);
 	for(int i = 0; i < NumberOfFATEntries; i++)
 	{
-		printf("FatEntry[ %d ] = %d \n", i, FAT[i]);
+		printf("FatEntry[ %d ] = %x \n", i, FAT[i]);
 	}
 	*/
 
@@ -289,6 +507,10 @@ int main()
 	printf("==========  FILE SYSTEM ===========\n\n");
 	char tab[100] = "";
 	displayDIREntries(tab, DIR, FAT);
+
+
+
+	//createDir(disk, "MACHO", &bpb, FAT, NumberOfFATEntries, bpb.RootClus);
 
 	//DIR LIST TO BE CLEARED
 	free(FAT);
